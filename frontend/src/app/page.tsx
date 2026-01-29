cd0956d88334e12395ea1c71d89ffd2406500a77
stanleyarmstrong <stanleyarmstrong31@gmail.com>
2026-01-28 22:04:56 -0800

    fix typo

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -62,7 +62,7 @@ export default function Home() {
             </div>
           </div>
           <div className='w-full lg:flex-1 max-w-4xl'>
-            <GitHubCalendar username="stanleyarmstrong" cheme={darkMode ? 'dark' : 'light'} />
+            <GitHubCalendar username="stanleyarmstrong" theme={darkMode ? 'dark' : 'light'} />
               {content?.intro?.map((paragraph, index) => (
                 <p className='text-md mt-5' key={`intro-${index}`}>{paragraph}</p>
               ))}
